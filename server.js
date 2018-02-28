50021ca7163fb38479d05170dade8bcc175d7c56
junghy <sun6500@naver.com>
2018-03-01 00:56:52 +0900

    20180301
    라우터 코드 추가
    홈페이지 페이지 추가

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 var server = app.listen(3000, function() {
-    console.log("텔코인 업무일지 서버");
+    console.log("Express server has started on port 3000")
 });

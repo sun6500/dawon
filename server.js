f8ec71051d9c2a29fe38f1ec75003c7fa290e67b
junghy <sun6500@naver.com>
2018-03-01 20:18:08 +0900

    20180301
    일부 파일 수정

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(3000, function() {
     console.log("Express server has started on port 3000!!");
-    app.use(express.static('Style'));
+    app.use(express.static('public'));
 });
